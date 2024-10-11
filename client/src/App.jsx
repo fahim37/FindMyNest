99fb0db8b70e267dc76a6eafff3e753edfea0db6
Fahim Ahmed Emon <ahmed.fahim37@gmail.com>
2024-10-11 23:17:07 +0600

    Header update and profile private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Home from "./pages/Home";
 import Header from "./components/Header";
+import PrivatrRoute from "./components/PrivatrRoute";
 
 export default function App() {
   return (
@@ -15,7 +16,9 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route element={<PrivatrRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );

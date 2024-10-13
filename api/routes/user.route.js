fa0aa81c69c0544b5b06fdd24186fd5650b2fb27
Fahim Ahmed Emon <ahmed.fahim37@gmail.com>
2024-10-14 01:06:51 +0600

    Listing page

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,6 +3,7 @@ import {
   deleteUser,
   updateUser,
   getUserListings,
+  getUser,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
 router.get("/listings/:id", verifyToken, getUserListings);
+router.get("/:id", verifyToken, getUser);
 
 export default router;

b9a5df26687400eebcf1798631edfba0a1805b36
Fahim Ahmed Emon <ahmed.fahim37@gmail.com>
2024-10-14 00:24:02 +0600

    update listing functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Home from "./pages/Home";
 import Header from "./components/Header";
 import PrivatrRoute from "./components/PrivatrRoute";
 import CreateListing from "./pages/CreateListing";
+import UpdateListing from "./pages/UpdateListing";
 
 export default function App() {
   return (
@@ -20,6 +21,10 @@ export default function App() {
         <Route element={<PrivatrRoute />}>
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
+          <Route
+            path="/update-listing/:listingId"
+            element={<UpdateListing />}
+          />
         </Route>
       </Routes>
     </BrowserRouter>

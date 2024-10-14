dd36be2de645e5c0ddae658413f642d5e7954b41
Fahim Ahmed Emon <ahmed.fahim37@gmail.com>
2024-10-15 00:02:21 +0600

    minor fix

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -90,10 +90,10 @@ export default function Listing() {
           )}
           <div className="flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4">
             <p className="text-2xl font-semibold">
-              {listing.name} - ${" "}
+              {listing.name} - ৳{" "}
               {listing.offer
-                ? listing.discountPrice.toLocaleString("en-US")
-                : listing.regularPrice.toLocaleString("en-US")}
+                ? listing.discountPrice.toLocaleString("en-IN")
+                : listing.regularPrice.toLocaleString("en-IN")}
               {listing.type === "rent" && " / month"}
             </p>
             <p className="flex items-center mt-6 gap-2 text-slate-600  text-sm">

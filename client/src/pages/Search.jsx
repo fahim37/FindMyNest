e3e411c364ea693f91dcdff5c2722e5d27aa11b7
Fahim Ahmed Emon <ahmed.fahim37@gmail.com>
2024-10-14 18:27:39 +0600

    Listing item components and show listings

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import ListingItem from "../components/ListingItem";
 
 const Search = () => {
   const [sidebardata, setSidebardata] = useState({
@@ -206,10 +207,25 @@ const Search = () => {
         </form>
       </div>
       {/* rightside------------------------------ */}
-      <div>
+      <div className="flex-1">
         <h1 className="text-3xl font-semibold p-3 border-b-2 mt-4">
           Available Listings:
         </h1>
+        <div className="p-7 flex flex-wrap ">
+          {!loading && listings.length === 0 && (
+            <p className="text-xl">No Listing Found</p>
+          )}
+          {loading && (
+            <p className="text-xl text-slate-700 text-center w-full">
+              Loading...
+            </p>
+          )}
+          {!loading &&
+            listings &&
+            listings.map((listing) => (
+              <ListingItem listing={listing} key={listing._id} />
+            ))}
+        </div>
       </div>
     </div>
   );

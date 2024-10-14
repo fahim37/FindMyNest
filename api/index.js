d3ed11722fe565ecf204cfa5a429b6217147ad98
Fahim Ahmed Emon <ahmed.fahim37@gmail.com>
2024-10-15 01:45:29 +0600

    server wait

--- api/index.js
+++ api/index.js
@@ -41,6 +41,9 @@ app.use((err, req, res, next) => {
     message,
   });
 });
+const server = http.createServer(app);
+server.keepAliveTimeout = 120000; // 120 seconds
+server.headersTimeout = 120000; // 120 seconds
 
 app.listen(process.env.PORT || 3000, "0.0.0.0", () => {
   console.log(`Server is running on port ${process.env.PORT || 3000}`);

b66ce1624e21f1496997655069d25f7ecf7919ec
Fahim Ahmed Emon <ahmed.fahim37@gmail.com>
2024-10-15 01:04:43 +0600

    server fix

--- api/index.js
+++ api/index.js
@@ -42,6 +42,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("server is running");
+app.listen(process.env.PORT || 3000, "0.0.0.0", () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
 });
